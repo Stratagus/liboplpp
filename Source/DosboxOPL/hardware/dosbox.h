6bd1469182633eacf2138179cc3112ea698768af
Daniel Hodgson <daniel.hodgson@codeprogrammers.net>
2013-09-14 01:35:10 -0700

    *Added programs.cpp to DosboxOPL build target *Added Section and Config classes to dosbox.h

--- Source/DosboxOPL/hardware/dosbox.h
+++ Source/DosboxOPL/hardware/dosbox.h
@@ -8,6 +8,11 @@ void E_Exit(const char * message,...) GCC_ATTRIBUTE( __format__(__printf__, 1, 2
 void MSG_Add(const char*,const char*); //add messages to the internal langaugefile
 const char* MSG_Get(char const *);     //get messages from the internal langaugafile
 
+class Section;
+
+class Config;
+extern Config * control;
+
 enum MachineType {
 	MCH_HERC,
 	MCH_CGA,
